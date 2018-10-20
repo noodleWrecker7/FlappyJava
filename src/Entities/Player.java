0c1bb4d1e54a27cafdd22e08e1c81ff03bf7c5d4
noodleWrecker7 <hodgkinson.adam@gmail.com>
2018-10-20 17:46:02 +0100

    called fps from Game, added a bit of functionality to jump and implemented gravity. Created collision method. Also some weird formatting

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -9,25 +9,36 @@ import static java.awt.Color.*;
 public class Player {
 
     private final int x = 50;
-    private int y;
-    private final int g, w, h;
+    private int y, ySpeed;
+    private final int g, w, h, jumpSpeed;
+    private static final int fps = Game.fps;
 
 
     public Player(int width, int height) {
         this.y = 250;
-        this.g = 20;
+        this.g = 1;
         this.w = width;
         this.h = height;
+        this.ySpeed = 0;
+        this.jumpSpeed = -20;
 
     }
 
     public void jump() {
+        this.ySpeed = jumpSpeed;
+    }
+
+    public void update(){
+        this.y += this.ySpeed;
+        this.ySpeed += g; // gravity
+    }
+
+    public void collision(){
 
     }
 
     public void render(Graphics g) {
         g.setColor(red);
-        g.fillRect(this.x, this.y, w, h);
+        g.fillRect(this.x, this.y, this.w, this.h);
     }
-
 }
